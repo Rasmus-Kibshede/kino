b654f1a177e078c16bf3a304d2ce24ee7eed1e8a
Rasmus Kibshede <2740ralle@gmail.com>
2023-12-06 13:58:28 +0100

    added: new version of js

--- server/index.js
+++ server/index.js
@@ -1,34 +1,50 @@
-import express from "express";
+import express from 'express';
 const app = express();
 
-import cors from "cors";
+import cors from 'cors';
 app.use(cors());
 
-const movies = [
-    {
-        id: 1,
-        title: "The Godfather",
-        director: "Francis Ford Coppola",
-        genre: "Crime"
-    },
-    {
-        id: 2,
-        title: "Star Wars",
-        director: "George Lucas",
-        genre: "Science Fiction"
-    },
-    {
-        id: 3,
-        title: "The Lord of the Rings",
-        director: "Peter Jackson",
-        genre: "Fantasy"
-    }
+// const movies = [
+//     {
+//         id: 1,
+//         title: "The Godfather",
+//         director: "Francis Ford Coppola",
+//         genre: "Crime"
+//     },
+//     {
+//         id: 2,
+//         title: "Star Wars",
+//         director: "George Lucas",
+//         genre: "Science Fiction"
+//     },
+//     {
+//         id: 3,
+//         title: "The Lord of the Rings",
+//         director: "Peter Jackson",
+//         genre: "Fantasy"
+//     }
+// ];
+
+const cards = [
+  {
+    suit: 'Heart',
+    rank: 'Ace',
+    person: 'You',
+    action: 'Guess',
+    object: 'Card',
+  },
+  {
+    suit: 'Spades',
+    rank: 'King',
+    person: 'Hitler',
+    action: 'He jumps',
+    object: 'Ball',
+  },
 ];
 
-
-app.get("/movies", (req, res) => {
-    res.send(movies);
+app.get('/cards', (req, res) => {
+  res.send(cards[Math.floor(Math.random() * cards.length)]);
 });
 
 const PORT = 8080;
-app.listen(PORT, () => console.log("The server is running on port", PORT));
+app.listen(PORT, () => console.log('The server is running on port', PORT));
